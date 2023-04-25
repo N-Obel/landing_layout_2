73ae43451ad790fad48328daef52cd34262e4248
Nikita <n.obelets@gmail.com>
2023-04-25 12:57:32 +0300

    finish

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.slider-main-block', {
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.body-main-block__arrow.swiper-button-next',
+		prevEl: '.body-main-block__arrow.swiper-button-prev',
+	},
+
+});
